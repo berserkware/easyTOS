c3b8057e51c89663fb0cd6d1c280850b8148b0bb
Berserkware <caleb@berserkware.org>
2024-04-16 15:56:01 +1200

    Updated screenshot, moved to using classes

--- easytos.py
+++ easytos.py
@@ -10,130 +10,153 @@ import subprocess
 VM_MEMORY = 4096
 VM_STORAGE = '5G'
 MOUNT_POINT = '/mnt/templeos'
+ISO_SOURCE = 'https://templeos.org/Downloads/TempleOS.ISO'
 
-def do_run():
-    """Runs TempleOS."""
-    os.system(f'sudo qemu-system-x86_64 -m {VM_MEMORY} -hda /var/lib/easytos/templeos.qcow2')
+def get_iso():
+    """Downloads the TempleOS ISO."""
+    response = requests.get(ISO_SOURCE)
+    with open('/var/lib/easytos/TempleOS.ISO', mode='wb') as file:
+        file.write(response.content)
 
-def do_install():
-    """Gets the TempleOS ISO, creates the QEMU Image, and runs it with the CD installed."""
-    if not os.path.exists('/var/lib/easytos/TempleOS.ISO'):
-        response = requests.get('https://templeos.org/Downloads/TempleOS.ISO')
-        with open('/var/lib/easytos/TempleOS.ISO', mode='wb') as file:
-            file.write(response.content)
 
-    if not os.path.exists('/var/lib/easytos/templeos.qcow2'):
-        os.system(f'sudo qemu-img create /var/lib/easytos/templeos.qcow2 {VM_STORAGE}')
+class SudoNotice(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
 
-    os.system(f'sudo qemu-system-x86_64 -boot d -cdrom /var/lib/easytos/TempleOS.ISO -m {VM_MEMORY} -hda /var/lib/easytos/templeos.qcow2')
-
-def do_mount():
-    """Mounts the QEMU disc."""
-    if not os.path.exists(MOUNT_POINT):
-        os.mkdir(MOUNT_POINT)
-
-    os.system('modprobe nbd max_part=8')
-    os.system('qemu-nbd --connect=/dev/nbd0 /var/lib/easytos/templeos.qcow2')
-    os.system('mount /dev/nbd0p1 /mnt/templeos')
+        self.message = ttk.Label(
+            self,
+            text='easyTOS needs superuser permissions to run.',
+        )
+        self.message.pack()
+    
+        self.button = ttk.Button(
+            self,
+            text='Ok',
+            command=args[0].destroy,
+            width=40,
+        )
+        self.button.pack()
         
-def do_unmount():
-    """Unmounts the QEMU disc."""
-    os.system(f'umount {MOUNT_POINT}')
-    os.system('qemu-nbd --disconnect /dev/nbd0')
-    os.system('rmmod nbd')
-    os.rmdir(MOUNT_POINT)
+    
+class MainMenu(tk.Frame):
+    def __init__(self, *args, **kwargs):
+        tk.Frame.__init__(self, *args, **kwargs)
+
+        self.message = ttk.Label(
+            root,
+            text='easyTOS!',
+            font=('Helvetica', 30),
+        )
+        self.message.pack(pady=8)
+
+        self.run_button = ttk.Button(
+            self,
+            text='RUN',
+            command=self.do_run,
+            width=30,
+        )
+        if os.path.exists('/var/lib/easytos/templeos.qcow2'):
+            self.run_button.pack()
+            
+        self.install_button = ttk.Button(
+            self,
+            text='INSTALL/REINSTALL',
+            command=self.do_install,
+            width=30,
+        )
+        self.install_button.pack()
+
+        self.mount_button = ttk.Button(
+            self,
+            text='MOUNT',
+            command=self.do_mount,
+            width=30,
+        )
+        if(os.path.exists('/var/lib/easytos/templeos.qcow2') and
+           not os.path.exists(MOUNT_POINT)):
+            self.mount_button.pack()
+
+        self.unmount_button = ttk.Button(
+            self,
+            text='UNMOUNT',
+            command=self.do_unmount,
+            width=30,
+        )
+        if os.path.exists(MOUNT_POINT):
+            self.unmount_button.pack()
+
+        self.exit_button = ttk.Button(
+            self,
+            text='EXIT',
+            command=args[0].destroy,
+            width=30,
+        )
+        self.exit_button.pack()
+
+    def do_run(self):
+        """Runs TempleOS."""
+        subprocess.Popen(
+            f'sudo qemu-system-x86_64 -m {VM_MEMORY} -hda /var/lib/easytos/templeos.qcow2',
+            shell=True,
+        )
+
+    def do_install(self):
+        """Gets the TempleOS ISO, creates the QEMU Image, and runs it with the CD installed."""
+        if not os.path.exists('/var/lib/easytos/TempleOS.ISO'):
+            get_iso()
+
+        if not os.path.exists('/var/lib/easytos/templeos.qcow2'):
+            os.system(f'sudo qemu-img create /var/lib/easytos/templeos.qcow2 {VM_STORAGE}')
+
+        os.system(
+            f'sudo qemu-system-x86_64 -boot d -cdrom /var/lib/easytos/TempleOS.ISO -m {VM_MEMORY} -hda /var/lib/easytos/templeos.qcow2'
+        )
+
+        self.exit_button.pack_forget()
+        self.run_button.pack()
+        self.mount_button.pack()
+        self.exit_button.pack()
+
+    def do_mount(self):
+        """Mounts the QEMU disc."""
+        if not os.path.exists(MOUNT_POINT):
+            os.mkdir(MOUNT_POINT)
+
+        os.system('modprobe nbd max_part=8')
+        os.system('qemu-nbd --connect=/dev/nbd0 /var/lib/easytos/templeos.qcow2')
+        os.system('mount /dev/nbd0p1 /mnt/templeos')
+
+        self.exit_button.pack_forget()
+        self.unmount_button.pack()
+        self.mount_button.pack_forget()
+        self.exit_button.pack()
         
-def do_main_window():
-    """Runs the program."""
+    def do_unmount(self):
+        """Unmounts the QEMU disc."""
+        os.system(f'umount {MOUNT_POINT}')
+        os.system('qemu-nbd --disconnect /dev/nbd0')
+        os.system('rmmod nbd')
+        os.rmdir(MOUNT_POINT)
+
+        self.exit_button.pack_forget()
+        self.unmount_button.pack_forget()
+        self.mount_button.pack()
+        self.exit_button.pack()
+
+    
+if __name__ == '__main__':
     root = tk.Tk()
     root.title('easyTOS');
     root.resizable(False, False)
 
-    message = ttk.Label(
-        root,
-        text='easyTOS!',
-        font=('Helvetica', 30),
-    )
-    message.pack()
-
-    if os.path.exists('/var/lib/easytos/templeos.qcow2'):
-        button = ttk.Button(
-            root,
-            text='RUN',
-            command=do_run,
-            width=30,
-        )
-        button.pack()
-
-    button = ttk.Button(
-        root,
-        text='INSTALL/REINSTALL',
-        command=do_install,
-        width=30,
-    )
-    button.pack()
-
-    if(os.path.exists('/var/lib/easytos/templeos.qcow2') and
-       not os.path.exists(MOUNT_POINT)):
-        button = ttk.Button(
-            root,
-            text='MOUNT',
-            command=do_mount,
-            width=30,
-        )
-        button.pack()
-
-    if os.path.exists(MOUNT_POINT):
-        button = ttk.Button(
-            root,
-            text='UNMOUNT',
-            command=do_unmount,
-            width=30,
-        )
-        button.pack()
-
-    """
-    Will try to fix later
-    button = ttk.Button(
-        root,
-        text='CONFIGURE EASYTOS',
-        command=lambda: os.system(f'xdg-open {__file__}'),
-        width=30,
-    )
-    button.pack()
-    """
-    
-    root.mainloop()
-
-def need_sudo():
-    """Runs a window containing a notice that the program needs sudo."""
-    root = tk.Tk()
-    root.title('easyTOS!');
-
-    root.resizable(False, False)
-
-    message = ttk.Label(
-        root,
-        text='easyTOS needs superuser permissions to run.',
-    )
-    message.pack()
-    
-    button = ttk.Button(
-        root,
-        text='Ok',
-        command=root.destroy,
-        width=40,
-    )
-    button.pack()
-
-    root.mainloop()
-    
-if __name__ == '__main__':
     if os.geteuid() != 0:
-        need_sudo()
-        sys.exit()
+        sudo_notice = SudoNotice(root)
+        sudo_notice.pack(side="top", fill="both", expand=True)
+    else:
+        if not os.path.isdir('/var/lib/easytos'):
+            os.mkdir('/var/lib/easytos')
+            
+        main_menu = MainMenu(root)
+        main_menu.pack(side="top", fill="both", expand=True)
 
-    if not os.path.isdir('/var/lib/easytos'):
-        os.mkdir('/var/lib/easytos')
-    
-    do_main_window()
+    root.mainloop()
